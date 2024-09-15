06e4682e8cb0637c7c7b57156852d5ef13cbcec0
codehunt022 <arunnath055@gmail.com>
2024-09-15 23:55:29 +0530

    node

---
+++ routes/url.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { handleGenerateNewShortUrl }= require("../controllers/url");
+const router = express.Router();
+
+router.post("/",handleGenerateNewShortUrl);
+module.exports = router;
+/* const express = require("express");
+const { handleGenerateNewShortUrl }= require("../controllers/url");
+const router = express.Router();
+
+router.post("/",handleGenerateNewShortUrl);
+module.exports = router;
+ */
